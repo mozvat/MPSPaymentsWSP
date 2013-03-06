2ba927241a8a2fadf5706adc87e2e1339fa2d120
Ozvat <mozvat@gmail.com>
2013-03-06 07:33:48 -0700

    First Commit

---
+++ WebServicesPlatform/Credit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace WebServicesPlatform
+{
+    public static class Credit
+    {
+        public static string Process(string dsiCreditRequest, string transactionPassword)
+        {
+            string rawResult = string.Empty;
+            string sanitizedResult = string.Empty;
+            MPS_WebServices.wsSoapClient proxy = new MPS_WebServices.wsSoapClient();
+            try
+            {
+                proxy.Endpoint.Address = new EndpointAddress(ProcessingEnvironment.Instance.EndPoint);
+                rawResult = proxy.CreditTransaction(dsiCreditRequest.Trim(), transactionPassword);
+                if (Response.Approved(rawResult))
+                {
+                    sanitizedResult = Response.SanitizeXMLResults(rawResult);
+                }
+                else //This will return a reason of the non-approval
+                {
+                    sanitizedResult = rawResult;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            return sanitizedResult;
+        }
+    }
+}
---
+++ WebServicesPlatform/InvoiceNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebServicesPlatform
+{
+    public static class InvoiceNumber
+    {
+        public static string New(string registrationNumber)
+        {
+            Random rand = new Random();
+            int randNumber = rand.Next(9999);
+            return registrationNumber + randNumber.ToString();
+        }
+
+    }
+}
