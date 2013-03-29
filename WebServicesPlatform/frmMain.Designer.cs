9b12a949cc794802d8f8c422ff9367c89dbdb6ce
mozvat <mozvat.gmail.com>
2013-03-29 12:19:48 -0600

    Added Main Form

---
+++ WebServicesPlatform/frmMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WebServicesPlatform
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreditSale = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreditSale
+            // 
+            this.btnCreditSale.Location = new System.Drawing.Point(28, 22);
+            this.btnCreditSale.Name = "btnCreditSale";
+            this.btnCreditSale.Size = new System.Drawing.Size(103, 53);
+            this.btnCreditSale.TabIndex = 0;
+            this.btnCreditSale.Text = "Credit Sale";
+            this.btnCreditSale.UseVisualStyleBackColor = true;
+            this.btnCreditSale.Click += new System.EventHandler(this.btnCreditSale_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(157, 102);
+            this.Controls.Add(this.btnCreditSale);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmMain";
+            this.Text = "Transactions";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreditSale;
+    }
+}
+
---
+++ WebServicesPlatform/frmMain.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WebServicesPlatform
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCreditSale_Click(object sender, EventArgs e)
+        {
+            string invoiceNumber = InvoiceNumber.New(DateTime.Now.Date.Day.ToString() + DateTime.Now.Date.Month.ToString() + DateTime.Now.Date.Year);
+            StringBuilder builder = new StringBuilder();
+            builder.Append("<?xml version=\"" + "1.0\"?>");
+            builder.Append("<TStream>");
+            builder.Append("<Transaction>");
+           // builder.Append("<MerchantID>" + "595901" + "</MerchantID>");
+            builder.Append("<MerchantID>" + "88430100662" + "</MerchantID>");
+            builder.Append("<TranType>Credit</TranType>");
+            builder.Append("<TranCode>Sale</TranCode>");
+            builder.Append("<InvoiceNo>" + invoiceNumber + "</InvoiceNo>");
+            builder.Append("<RefNo>" + invoiceNumber + "</RefNo>");
+            builder.Append("<Memo>Discover Test</Memo>");
+            builder.Append("<Frequency>OneTime</Frequency>");
+            builder.Append("<RecordNo>RecordNumberRequested</RecordNo>");
+            builder.Append("<Account>");
+            builder.AppendFormat("<AcctNo>{0}</AcctNo>", "6221197626920643");
+            builder.AppendFormat("<ExpDate>{0}</ExpDate>", "1017");
+            builder.Append("</Account>");
+            builder.Append("<Amount>");
+            builder.Append("<Purchase>" + "3.50" + "</Purchase>");
+            builder.Append("</Amount>");
+            builder.Append("</Transaction>");
+            builder.Append("</TStream>");
+            //Attempt to process credit card
+            string creditResult = Credit.Process(builder.ToString(), "81122HER");
+        }
+    }
+}
